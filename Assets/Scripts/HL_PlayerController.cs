dc04364c7e644c7351d848f9ab18529209b69bb0
Harjot Singh <121034602+HarjotSinghGillHL@users.noreply.github.com>
2024-01-22 20:33:12 -0400

    .

---
+++ Assets/Scripts/HL_PlayerController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HL_PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rigidBody;
+
+    [SerializeField]
+    private int moveSpeed = 10000;
+
+    private Vector2 vecVelocity = Vector2.zero;
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+    void MovePlayer()
+    {
+        vecVelocity.x = (Input.GetAxis("Horizontal") * Time.deltaTime);
+        vecVelocity.y = (Input.GetAxis("Vertical") * Time.deltaTime);
+        vecVelocity *= moveSpeed;
+        rigidBody.velocity = vecVelocity;
+    }
+
+    void Update()
+    {
+        MovePlayer();
+    }
+}
